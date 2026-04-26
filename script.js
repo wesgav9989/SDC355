426c4f67823a28c44414a4708495ccf6c28002ba
wesgav9989 <wesgav9989@students.ecpi.edu>
2026-04-26 15:53:28 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var userName = prompt("Please enter your name:");
-
-if (userName !== null && userName.trim() !== "") {
-    document.getElementById("welcomeMessage").textContent = "Welcome, " + userName + "!";
-} else {
-    document.getElementById("welcomeMessage").textContent = "Welcome, guest!";
-}
